fd1c1195e2665ef60ea922b5a3a9ac51eee4d1d5
Sooraj Kumar <sskumar5@illinois.edu>
2021-05-01 10:21:07 -0400

    flask + mongodb integration

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, jsonify
-from prompt import returning_main
+# from prompt import returning_main
 import requests
 import json
 import openai
 import asyncio
-from flask_pymongo import PyMongo
-from flask import jsonify
 import db
 
-app = Flask(__name__,static_folder='./build', static_url_path='/')
+RESUME_DATA_KEY = "resume_data"
+
+app = Flask(__name__,
+            static_folder='./build',
+            static_url_path='/')
+
 
 @app.route('/')
 def index():
         return app.send_static_file('index.html')
 
+
+"""
 @app.route("/test", methods=['POST'])
 def test():
     json_file = request.data
@@ -23,6 +28,8 @@ def test():
     print(summary)
     print((summary), "hello")
     return "Connected to the data base!"
+"""
+
 
 @app.route("/users", methods=['GET'])
 def sendingreqtdata():
@@ -30,28 +37,65 @@ def sendingreqtdata():
     print(everything)
     
     output = []
-    for s in  everything:
-        output.append({'resume' : s['resume'], 'summary' : s['summary']})
+    for s in everything:
+        output.append({'resume': s['resume'], 'summary': s['summary']})
 
-    return jsonify({'result' : output})
+    return jsonify({'result': output})
+
+
+@app.route("/api/v2/resume/all", methods=['GET'])
+def get_all_resume_data():
+    try:
+        resp = db.resumeCollection.find({})
+        print("Resp: ", resp)
+        json_response = format_response_sucess_retreival(resp)
+    except Exception as e:
+        print("Error when retrieving data from mongodb resume collection", str(e))
+        json_response = {"ERROR": str(e)}
+
+    return jsonify(json_response)
+
+
+def format_response_sucess_retreival(response):
+    json_response = {"ids": []}
+    for _id in response:
+        json_response["ids"].append(_id["name"])
+    print("json_response", json_response)
+    return json_response
+
 
 @app.route("/api/v2/resume", methods=['POST'])
 def add_resume_data():
-    raw_resume_data = request.resume_data
-    # Parse resume data from the request
-    raw_resume_data = parse_resume_data()
-    # Open a connection to db
-    # Format data appropriately for db insertion
-    formatted_resume_data = format_data_for_insertion()
-    # Insert into db, include try-catch
+    raw_resume_data = json.loads(request.args.get(RESUME_DATA_KEY))
+    formatted_resume_data = format_data_for_insertion(raw_resume_data)
+    resp = {}
     try:
-        resp = insert_resume_data(formatted_resume_data)
+        resp = db.resumeCollection.insert_one(formatted_resume_data)
+        json_response = {"inserted_id": str(resp.inserted_id)}
     except Exception as e:
-        resp = handle_insertion_error(e)
-    # Return success/error codes as appropriate + add information for front-end
-    #   to process
-    return jsonify(resp)
+        print("Error when inserting resume data into mongodb resume collection: ", str(e))
+        json_response = {"ERROR": str(e)}
+    return jsonify(json_response)
 
+
+@app.route("/api/v2/user", methods=['POST'])
+def add_user():
+    pass
+
+
+@app.route("/api/v2/user", methods=['GET'])
+def get_user():
+    pass
+
+
+@app.route("/api/v2/user/all", methods=['GET'])
+def get_all_users():
+    pass
+
+
+def format_data_for_insertion(parsed_resume_json):
+    # @TODO: Add more logic if required
+    return parsed_resume_json
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,10 +1,84 @@
-from flask_pymongo import pymongo
+# from flask_pymongo import pymongo
+import pymongo
+import json
+
+DATA_DIR = "data"
+DATABASE = "ethnicityDb"
+USER_COLLECTION = "userCollection"
+RESUME_COLLECTION = "resumeCollection"
+JOBS_COLLECTION = "jobsCollection"
+TEST_COLLECTION = "testCollection"
+CONNECTION_STRING = "mongodb+srv://team10:A6u8bn4g9sYxSvfX@team10cluster.vkuqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+# CONNECTION_STRING = "mongodb+srv://Clariza_Mayo:Foreve6434!@cluster0.y9k04.mongodb.net/<ValtechSocialImpactHackathon>?retryWrites=true&w=majority"
+
+mongo_client = pymongo.MongoClient(CONNECTION_STRING, connect=True)
 
 
-CONNECTION_STRING = "mongodb+srv://Clariza_Mayo:Foreve6434!@cluster0.y9k04.mongodb.net/<ValtechSocialImpactHackathon>?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-#  db = client.get_database('flask_mongodb_atlas')
-#  user_collection = pymongo.collection.Collection(db, 'user_collection')
+def list_all_databases(client):
+    print(client.list_database_names())
 
-mydb = client["mydatabase"]
-userCollection = mydb["users"]
+
+def check_if_database_exists(client, db_name):
+    if db_name in client.list_database_names():
+        print(f"Db {db_name} exists")
+    else:
+        print(f"Db {db_name} doesn't exist")
+
+
+def list_all_collections(client, db_name):
+    print(client[db_name].list_collection_names())
+
+
+def check_if_collection_exists(client, db_name, collection_name):
+    if collection_name in client[db_name].list_collection_names():
+        print(f"Collection {collection_name} exists")
+        return True
+    else:
+        print(f"Collection {collection_name} doesn't exist")
+        return False
+
+
+def insert_dummy_data_into_collection(dummy_data_filename, collection_handler):
+    dummy_data_fullpath = DATA_DIR + '/' + dummy_data_filename
+    with open(dummy_data_fullpath) as data_f:
+        dummy_data = json.load(data_f)
+    resp = collection_handler.insert_one(dummy_data[0])
+    return resp.inserted_id
+
+
+_db = mongo_client[DATABASE]
+userCollection = _db[USER_COLLECTION]
+resumeCollection = _db[RESUME_COLLECTION]
+jobsCollection = _db[JOBS_COLLECTION]
+testCollection = _db[TEST_COLLECTION]
+
+user_collection_exists = check_if_collection_exists(mongo_client,
+                                                    DATABASE,
+                                                    USER_COLLECTION)
+
+if not user_collection_exists:
+    inserted_ids = insert_dummy_data_into_collection('dummy_test_data.json',
+                                                     userCollection)
+    print(f"Ids of inserted documents into {USER_COLLECTION}: {inserted_ids}")
+
+resume_collection_exists = check_if_collection_exists(mongo_client,
+                                                      DATABASE,
+                                                      RESUME_COLLECTION)
+if not resume_collection_exists:
+    inserted_ids = insert_dummy_data_into_collection('dummy_test_data.json',
+                                                     resumeCollection)
+    print(f"Ids of inserted documents into {RESUME_COLLECTION}: {inserted_ids}")
+
+jobs_collection_exists = check_if_collection_exists(mongo_client,
+                                                    DATABASE,
+                                                    JOBS_COLLECTION)
+
+if not jobs_collection_exists:
+    inserted_ids = insert_dummy_data_into_collection('dummy_test_data.json',
+                                                     jobsCollection)
+    print(f"Ids of inserted documents into {JOBS_COLLECTION}: {inserted_ids}")
+
+
+# Testing
+# list_all_databases(mongo_client)
+# list_all_collections(mongo_client, DATABASE)
